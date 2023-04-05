666909d55ba68b5456e8e6493cf592953f6024fc
Andre Ribeiro <andresmribeiro@gmail.com>
2023-04-05 15:24:30 +0100

    initial commit

---
+++ src/app/(outside)/admin/settings.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// components
+import CardSettings from "components/Cards/CardSettings.tsx";
+import CardProfile from "components/Cards/CardProfile.tsx";
+
+// layout for page
+import Admin from "layouts/Admin.tsx";
+
+export default function Settings() {
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-8/12 px-4">
+          <CardSettings />
+        </div>
+        <div className="w-full lg:w-4/12 px-4">
+          <CardProfile />
+        </div>
+      </div>
+    </>
+  );
+}
+
+Settings.layout = Admin;
---
+++ src/app/(outside)/admin/tables.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+// components
+import CardTable from "components/Cards/CardTable.tsx";
+
+// layout for page
+import Admin from "layouts/Admin.tsx";
+
+export default function Tables() {
+  return (
+    <>
+      <div className="flex flex-wrap mt-4">
+        <div className="w-full mb-12 px-4">
+          <CardTable />
+        </div>
+        <div className="w-full mb-12 px-4">
+          <CardTable color="dark" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+Tables.layout = Admin;
